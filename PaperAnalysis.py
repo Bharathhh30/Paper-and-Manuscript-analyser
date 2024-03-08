5eb34e244a64bb84c92cb7c3f89e628e30e49a7f
Bharathhh30 <155385140+Bharathhh30@users.noreply.github.com>
2024-03-08 16:43:38 +0530

    Update PaperAnalysis.py

--- PaperAnalysis.py
+++ PaperAnalysis.py
@@ -11,7 +11,7 @@ from googletrans import Translator
 translator = Translator()
 
 # Configure Google API for OpenAI
-genai.configure(api_key=os.getenv("AIzaSyB9P4tqNr9gdD_SEMg01SgCIIJGLGGYq0o"))
+genai.configure(api_key=os.getenv("GEMINI-API-KEY"))
 
 # Function to load Google Gemini Pro Vision API And get response
 def get_gemini_response(image, prompt):
